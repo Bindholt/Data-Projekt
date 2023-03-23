c077f25bc5309d3dc7b9ad5576142dc70f93cc69
Bindholt <Cbindholt@gmail.com>
2023-03-23 14:35:45 +0100

    lavet json fil (cartman.json)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-window.addEventListener("load", ready);
+window.addEventListener("load", readyPotter);
 
 function ready() {
     //fetchData()
@@ -16,7 +16,7 @@ function ready() {
         age: 10,
         voicedBy: "Trey Parker",
         gender: "Male",
-        religion: "Roman Catholic",
+        religion: "Roman Catholic", 
         catchPhrase: "Screw you guys, I'm going home!",
         hairColor: "Brown",
         schoolGrade: 4,
@@ -25,52 +25,80 @@ function ready() {
         firstAppearance: "S01E01",
     };
     addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
+    addCharacter(cartman);
 }
 
 function addCharacter(character) {
-    const characterHTML = /*html*/ `
-            <td>
-                <span>${character.name}</span>
-            </td>
-            <td>
-                <span>${character.nickname}</span>
-            </td>
-            <td>
-                <span>${character.occupation}</span>
-            </td>
-            <td>
-                <span>${character.age}</span>
-            </td>
-            <td>
-                <span>${character.voicedBy}</span>
-            </td>
-            <td>
-                <span>${character.gender}</span>
-            </td>
-            <td>
-                <span>${character.religion}</span>
-            </td>
-            <td>
-                <span>${character.catchPhrase}</span>
-            </td>
-            <td>
-                <span>${character.hairColor}</span>
-            </td>
-            <td>
-                <span>${character.schoolGrade}</span>
-            </td>
-            <td>
-                <span>${character.episodes}</span>
-            </td>
-            <td>
-                <span>${character.appearances}</span>
-            </td>
-            <td>
-                <span>${character.firstAppearance}</span>
-            </td>
-            <td>
+    document.querySelector("#characters").insertAdjacentHTML(
+        "beforeend",
+        /*html*/ `
+            <article>
                 <img src="${character.image}">
-            </td>
-    `
-    document.querySelector("#students").insertAdjacentHTML("beforeend", characterHTML);
+                <h2>${character.name}</h2>
+                <p>${character.occupation}</p>
+            </article>
+        `
+    );
+    
+    document.querySelector("#characters article:last-child").addEventListener("click", characterClicked);
+
+    function characterClicked() {
+        showClickedCharacter(character);
+    }
+}
+
+function showClickedCharacter(character) {
+    document.querySelector("#dialog-image").src = character.image;
+    document.querySelector("#dialog-name").textContent = character.name;
+    document.querySelector("#dialog-name2").textContent = character.name;
+    document.querySelector("#dialog-gender").textContent = character.gender;
+    document.querySelector("#dialog-occupation").textContent = character.occupation;
+    document.querySelector("#dialog-age").textContent = character.age;
+    document.querySelector("#dialog-religion").textContent = character.religion;
+    document.querySelector("#dialog-nickname").textContent = character.nickname;
+    document.querySelector("#dialog-catch-phrase").textContent = character.catchPhrase;
+    document.querySelector("#dialog-hair-color").textContent = character.hairColor;
+    document.querySelector("#dialog-school-grade").textContent = character.schoolGrade;
+    document.querySelector("#dialog-episodes").textContent = character.episodes;
+    document.querySelector("#dialog-appearances").textContent = character.appearances;
+    document.querySelector("#dialog-first-appearance").textContent = character.firstAppearance;
+    document.querySelector("#dialog-voiced-by").textContent = character.voicedBy;
+
+    document.querySelector("#dialog-character").showModal();
+}
+
+
+
+
+
+
+
+/*------------------ POTTER -------------------------------*/
+
+async function readyPotter() {
+    const harry = await getCharacter("cartman.json");
+
+    console.log(harry);
+    addCharacter(harry);
+
+}
+
+async function getCharacter(url) { 
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+{
+    
 }
